e8ce49155f1c3699ae8132c91ae8c6353d6708ba
iamabdirazak <a.razakmubarak@icloud.com>
2026-03-14 16:31:30 +0300

    add all data types

---
+++ data_types.py
@@ -0,0 +1,21 @@
+# Math operations with variables
+a = 10
+b = 5.5
+sum_result= a + b
+print(f"The sum of {a} and {b} is {sum_result}")
+
+# comparing division and integer division
+division_result = a / b
+integer_division_result: int = a // b
+print(f"The result of {a} divided by {b} is {division_result}")
+print(f"The result of {a} integer divided by {b} is {integer_division_result}")
+
+c = "Hello"
+d = True
+e = None
+
+print(type(a))  # Output: <class 'int'>
+print(type(b)) # Output: <class 'float'>
+print(type(c))  # Output: <class 'str'>
+print(type(d))  # Output: <class 'bool'>
+print(type(e))  # Output: <class 'NoneType'>
